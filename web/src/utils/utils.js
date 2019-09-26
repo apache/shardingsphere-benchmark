e6e1fb4127ac3ae69da6eb74e84970dc76fbf69c
wqzwh <wangziqihappy@163.com>
2019-09-26 15:28:22 +0800

    feature:add ui-frontend project

---
+++ web/src/utils/utils.js
@@ -0,0 +1,11 @@
+import mysqlVsShardingproxy from '../../../report/data_json/mysql_vs_shardingproxy.json'
+import shardingProxyMasterSlaveSharding from '../../../report/data_json/sharding_proxy_master_slave_sharding.json'
+import shardingProxyMasterSlave from '../../../report/data_json/sharding_proxy_master_slave.json'
+import shardingProxySingleDatabaseSingleTable from '../../../report/data_json/sharding_proxy_single_database_single_table.json'
+
+export {
+  mysqlVsShardingproxy,
+  shardingProxyMasterSlaveSharding,
+  shardingProxyMasterSlave,
+  shardingProxySingleDatabaseSingleTable
+}
---
+++ web/src/views/sharding-proxy-master-slave/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <Row :gutter="16" style="background:#eee; padding:20px">
+    <Col v-for="(value, name, index) in series" :key="index" class="col-item" span="24">
+      <Card :bordered="false">
+        <p slot="title" style="font-size: 18px">{{ name.toLowerCase() }} throughtout compare</p>
+        <v-chart :options="getOptions(name)" />
+        <Form :label-width="140">
+          <FormItem
+            v-for="(value, name,indx) in desc[name]"
+            :label="`${name}:`"
+            :key="indx"
+          >{{ value }}</FormItem>
+        </Form>
+      </Card>
+    </Col>
+  </Row>
+</template>
+
+<script>
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/title'
+import { shardingProxyMasterSlave } from '../../utils/utils'
+import { mountedMixin } from '../../utils/mixin'
+import { getLineOptions } from '../../utils/line'
+
+export default {
+  name: 'Home',
+  components: {
+    'v-chart': ECharts
+  },
+  mixins: [mountedMixin],
+  data() {
+    return {
+      data: shardingProxyMasterSlave
+    }
+  },
+  methods: {
+    getOptions(name) {
+      return getLineOptions(name, this.xAxis, this.legend, this.series)
+    }
+  }
+}
+</script>
+<style>
+.echarts {
+  width: 100%;
+  height: 400px;
+}
+.col-item {
+  margin-bottom: 16px;
+}
+.ivu-form-item {
+  margin-bottom: 10px !important;
+}
+.ivu-form-item-label {
+  font-size: 15px !important;
+}
+.ivu-form-item-content {
+  font-size: 14px !important;
+}
+</style>
