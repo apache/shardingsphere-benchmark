9a02b5d11778cfcc95b7578cc0c6626c4af978d2
wqzwh <wangziqihappy@163.com>
2019-10-21 12:00:46 +0800

    feature:modify api route

--- shadingsphere-benchmark-ui/src/utils/utils.js
+++ shadingsphere-benchmark-ui/src/utils/utils.js
@@ -2,15 +2,15 @@ import API from './api'
 
 export default {
   getMysqlVsShardingData: () =>
-    API.get(`/data_json/mysql_vs_shardingproxy.json`),
+    API.get(`/data_json/mysql_vs_sharding-proxy.json`),
   getShardingProxyMasterSlaveShardingData: () =>
-    API.get(`/data_json/sharding_proxy_master_slave_sharding.json`),
+    API.get(`/data_json/sharding-proxy_master_slave_sharding.json`),
   getShardingProxyMasterSlaveData: () =>
-    API.get(`/data_json/sharding_proxy_master_slave.json`),
+    API.get(`/data_json/sharding-proxy_master_slave.json`),
   getShardingProxySingleDatabaseSingleTableData: () =>
-    API.get(`/data_json/sharding_proxy_single_database_single_table.json`),
+    API.get(`/data_json/sharding-proxy_single_database_single_table.json`),
   getShardingjdbcVsShardingproxyEncrypt: () =>
-    API.get(`/data_json/shardingjdbc_vs_shardingproxy_encrypt.json`),
+    API.get(`/data_json/sharding-jdbc_vs_sharding-proxy_encrypt.json`),
   getShardingjdbcVsShardingproxyShardingEncrypt: () =>
-    API.get(`/data_json/shardingjdbc_vs_shardingproxy_sharding_encrypt.json`)
+    API.get(`/data_json/sharding-jdbc_vs_sharding-proxy_sharding_encrypt.json`)
 }
