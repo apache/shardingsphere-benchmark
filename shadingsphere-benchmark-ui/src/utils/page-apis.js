b1f4a3ebd8b4f64e9de754fe7b45a3ea2426ac54
wqzwh <wangziqihappy@163.com>
2019-10-28 13:27:18 +0800

    feature:Update chart display field content

--- shadingsphere-benchmark-ui/src/utils/page-apis.js
+++ shadingsphere-benchmark-ui/src/utils/page-apis.js
@@ -1,13 +1,13 @@
 import API from './api'
 
 export default {
-  getFileList: () => API.get(`/data_json/file_list.json`),
+  getFileList: () => API.get(`/jtl_json/file_list.json`),
   getEncryptShardingTestData: () =>
-    API.get(`/data_json/encrypt_sharding_test.json`),
-  getLossTestData: () => API.get(`/data_json/loss_test.json`),
-  getEncryptTestData: () => API.get(`/data_json/encrypt_test.json`),
+    API.get(`/jtl_json/encrypt_sharding_test.json`),
+  getLossTestData: () => API.get(`/jtl_json/loss_test.json`),
+  getEncryptTestData: () => API.get(`/jtl_json/encrypt_test.json`),
   getMasterSlaveShardingTestData: () =>
-    API.get(`/data_json/master_slave_sharding_test.json`),
-  getMasterSlaveTest: () => API.get(`/data_json/master_slave_test.json`),
-  getShardingTest: () => API.get(`/data_json/sharding_test.json`)
+    API.get(`/jtl_json/master_slave_sharding_test.json`),
+  getMasterSlaveTest: () => API.get(`/jtl_json/master_slave_test.json`),
+  getShardingTest: () => API.get(`/jtl_json/sharding_test.json`)
 }
